8688811d490095409838374d429b22aeca39a32b
LacamJC <joaoramajo744@gmail.com>
2025-01-23 14:14:40 -0300

    ADD - Responsividade no  cabecalho

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,17 @@
 import styles from '../../assets/scss/layout/Header.module.css'
-import logo from '../../assets/img/logo_completa.webp'
+import logo from '../../assets/img/newLogo.webp'
+import { useState } from 'react'
+import { CiMenuBurger } from "react-icons/ci"
 const Header = () =>{
+
+    const [showMenu, setShowMenu] = useState(true)
+
+    const handleChange = () =>{
+        
+        setShowMenu(!showMenu)
+
+    }
+
     return(
         <>
             <header className={`${styles.cabecalho}`}>
@@ -18,6 +29,23 @@ const Header = () =>{
                 </nav>
 
                 <button className={`${styles.cta_coleta}`}>Solicite sua coleta</button>
+
+                {/* HIDDEN MENU (Para telas menores que 920px) */}
+               
+
+                <nav className={`${styles.hidden_navBar} ${showMenu?`${styles.show}`:`${styles.hidden}`}`}>
+                <ul className={`${styles.navBar_list} `}>
+                        <li className={`${styles.list_item}`}>Home</li>
+                        <li className={`${styles.list_item}`}>Serviços</li>
+                        <li className={`${styles.list_item}`}>Sobre Nós</li>
+                        <li className={`${styles.list_item}`}>Contato</li>
+{/* Instagram (Ícone/link para o Instagram) */}
+                    </ul>
+                </nav>
+                <div className={`${styles.menu_controls}`}>
+                    <input type={"checkbox"} onChange={handleChange} id='checkbox' className={`${styles.checkbox_menu}`}></input>
+                    <CiMenuBurger className={`${styles.burguer_icon}`} />
+                </div>
             </header>
         </>
     )
