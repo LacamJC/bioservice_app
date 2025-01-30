6e9d2c9a9f2c429fb03de149d0666e5532ffed3f
LacamJC <joaoramajo744@gmail.com>
2025-01-30 12:05:13 -0300

    ADD

--- src/components/objects/Whatsapp.js
+++ src/components/objects/Whatsapp.js
@@ -3,7 +3,9 @@ import styles from '../../assets/scss/objects/Whatsapp.module.css'
 const Whatsapp = () =>{
     return(
         <>
-            <FaWhatsapp className={`${styles.icone}`} />
+            <a href="https://wa.me/+5511985322036?text=Ol%C3%A1%2C%20vim%20pelo%20seu%20site%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es" target="_BLANK" title="Entrar em contato pelo Whatsapp">
+                <FaWhatsapp className={`${styles.icone}`} />
+            </a>
         </>
     )
 }
